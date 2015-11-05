71165c06af9b22b907c565c6f1f37510b88acd08
peiyu <peiyuliu@gmail.com>
2015-11-04 22:56:13 -0800

    Initial Commit

---
+++ week 3 mailbox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  week 3 mailbox
+//
+//  Created by Peiyu Liu on 11/4/15.
+//  Copyright © 2015 Peiyu Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
